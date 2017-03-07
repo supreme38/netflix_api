eccd6e3f40ff8bc93e388d9674b3a6356baaf660
supreme38 <vince.liu38@gmail.com>
2017-03-07 10:26:27 -0500

    post working

--- client/app.js
+++ client/app.js
@@ -1,6 +1,11 @@
 angular.module('app', [])
 
-.controller('mainCtrl', function($scope) {
-  $scope.display = "test";
+.controller('mainCtrl', function($scope, $http) {
+  $scope.search = (data) => {
+		$http.post('/search', data).then((response) => {
+			$scope.list = (response.data)
+			console.log(response.data)
+		});
+	}
 
-})
+});
--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const rp = require('request-promise');
 const app = express();
 
 app.set('port', process.env.PORT || 8000);
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static('client'));
 
 app.post('/search', (req, res) => {
+
+	var title = req.body.title;
+	console.log('******************', title)
+
 	let options = {
 		uri: 'http://netflixroulette.net/api/api.php?',
 		qs: {
-			title: 'family%guy%'
+			title: title
 		},
 		json: true
 	};
@@ -19,24 +24,30 @@ app.post('/search', (req, res) => {
 	let options2 = {
 		uri: 'http://www.omdbapi.com/?',
 		qs: {
-			t: 'family%guy'
+			t: title,
+			r: 'json'
 		},
 		json: true
 	}
 
+	var obj = {};
+
 	rp(options)
 		.then((repos) => {
-			console.log(repos)
+			console.log("*****", repos)
+			obj['movie1'] = repos;
 		})
 		.then(rp(options2)
 			.then((repos) => {
-				res.json(repos)
+				obj['movie2'] = repos
+				res.send(obj)
 			})
 		)
 		.catch((err) => {
-			console.log(err)
+			throw(err)
 		});
 });
+
 
 app.listen(app.get('port'), () => {
 	console.log('Listening on port', app.get('port'))
